9b6b874032d374f76b73f434a7163c38473838df
Michelle-Zeng <mzeng9@illinois.edu>
2026-04-17 22:12:14 -0500

    update config with colors and font choices

--- config/config.py
+++ config/config.py
@@ -3,3 +3,12 @@ class Config:
     # general window properties
     window_height = 800
     window_width = 800
+
+    bg_color        = "#ffffff"
+    header_color    = "#2c3e6b"
+    button_color    = "#3a5fc8"
+    text_color      = "#000000"
+
+    font_title = ("Times New Roman", 20, "bold")
+    font_normal = ("Times New Roman", 15)
+    font_small = ("Times New Roman", 10)
